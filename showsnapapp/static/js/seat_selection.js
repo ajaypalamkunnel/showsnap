df34357e793e0c569ea3a927afabdbb9156b46b5
ajaypalamkunnel <ajaypalamkunnel45@gmail.com>
2024-03-03 13:55:43 +0530

    seat arrangement is correct input box data entery correct for name not not amount

--- showsnapapp/static/js/seat_selection.js
+++ showsnapapp/static/js/seat_selection.js
@@ -22,12 +22,25 @@ seats.forEach(seat => {
 });
 
 // Update selected seats list
+// Update selected seats list
 function updateSelectedSeatsList() {
-  const selectedSeatsList = document.getElementById('selected-seats');
-  selectedSeatsList.innerHTML = '';
-  selectedSeats.forEach(seat => {
-    const li = document.createElement('li');
-    li.textContent = `Seat: ${seat.name}, Amount: ${seat.price}`;
-    selectedSeatsList.appendChild(li);
+  const selectedSeatsInput = document.getElementById('selected-seats-input');
+  const totalAmountInput = document.getElementById('total-amount-input');
+  let selectedSeatsString = '';
+  let totalPrice = 0;
+  
+  selectedSeats.forEach((seat, index) => {
+    selectedSeatsString += seat.name;
+    totalPrice += seat.price; // Accumulate the total price
+    
+    // If it's not the last seat, add a comma separator
+    if (index < selectedSeats.length - 1) {
+      selectedSeatsString += ',';
+    }
   });
+  
+  // Set the selected seats string and total amount as the value of the input boxes
+  selectedSeatsInput.value = selectedSeatsString;
+  totalAmountInput.value = totalPrice;
 }
+
